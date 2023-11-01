8a7b2e43ea418a19c91acab2930f218a01cff026
Mike <synz23@gmail.com>
2023-11-02 00:23:58 +0800

    測試 ini 檔的練習

--- test/data.php
+++ test/data.php
@@ -1,49 +1,10 @@
 <?php
-$data = array(
-    "name" => "John",
-    "age" => 30,
-    "city" => "New York"
-);
-
-// 将 PHP 数组编码为 JSON 字符串
-$jsonData = json_encode($data);
-
-// 指定 JSON 文件的路径
-$filename = "data.json";
-
-// 打开文件以进行写入，如果文件不存在则创建
-$handle = fopen($filename, "w");
-
-if ($handle) {
-    // 写入 JSON 数据到文件
-    if (fwrite($handle, $jsonData) !== false) {
-        echo "JSON 数据已成功写入文件。";
-        fclose($handle); // 关闭文件句柄
-    } else {
-        echo "写入文件时出错。";
-    }
-} else {
-    echo "无法打开文件以进行写入。";
-}
-// 文件路径
-$filename = "data.json";
-
-// 读取文件内容
-$jsonData = file_get_contents($filename);
-
-// 解码 JSON 数据
-$data = json_decode($jsonData, true);
-
-if ($data !== null) {
-    // JSON 解码成功
-    echo $data["name"];
-} else {
-    // JSON 解码失败
-    echo "JSON 解码失败";
-}
-
-
-
-
-
+// 解析 config.ini 文檔
+$ini = parse_ini_file("config.ini", true);
+// 將 ini 陣列轉換成 json
+$json_ini = json_encode($ini);
+// 設置標頭
+header("Content-Type: application/json");
+// 回傳資料 (json 格式)
+echo $json_ini;
 ?>
--- test/main.js
+++ test/main.js
@@ -1,18 +1,25 @@
-let start = document.getElementById("start");
-let input = document.getElementById("input");
-let time = input.value * 1000;
-let sc;
-let timer = function () {
-  console.log("timer: " + time);
-};
-// 點擊事件
-start.addEventListener("click", function () {
-  time = input.value * 1000;
-  clearInterval(sc);
-  setInterval(timer, time);
-});
+// 初始化資料
+let datas;
+// 建立 ajax 新連線
+let request = new XMLHttpRequest();
+request.onreadystatechange = function () { // 當連線的狀態改變時
+  if (request.readyState == 4 && request.status == 200) {
+    let json_data = request.responseText; // 取得資料
+    // 將 json 字串解析為 javascript 物件
+    datas = JSON.parse(json_data);
+  }
+}
+// 指定連線路徑與傳送方式，關閉同步: false (程式會等完連線取得完回應才會進行下一步)
+request.open("GET", "data.php", false);
+// 發送請求
+request.send();
 
-// 在載入 html 後不必等待載入元件直接執行這裡的 javascript
-document.addEventListener("DOMContentLoaded", function () {
-  sc = setInterval(timer, time);
-});
+// 使用者名子
+let user_name = datas["user_information"]["user_name"];
+// 使用者電話
+let phone_number = datas["user_information"]["phone_number"];
+// 使用者地址
+let address = datas["user_address"]["user_address"];
+// 結果顯示
+let result = user_name + " 的電話號碼是: " + phone_number + "\n" + "地址是: " + address;
+console.log(result);
