30efa1cc7a4c8e2dd1bc1c0f87bbc6ca5fd0604d
Mike <synz23@gmail.com>
2023-11-21 17:22:56 +0800

    test 有 css 選項範例，待研究

--- test/data.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = [10, 60, 80, "我好帥"];
-// 將 data 陣列轉換成 json
-$json_data = json_encode($data);
-// 設置標頭
-header("Content-Type: application/json");
-// 回傳資料 (json 格式)
-echo $json_data;
-?>
--- test/main.js
+++
@@ -1,9 +0,0 @@
-var Fish = ["angel", "clown", "drum", "sturgeon"];
-var myFish = "drum";
-for (let i = 0; i < Fish.length; i++) {
-    if (myFish == Fish[i]) {
-        continue;
-    } else {
-        console.log(i);
-    }
-};
